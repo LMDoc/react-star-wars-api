d9ed6e224c5776c4d18daa72ac43f60879074717
Liam D <liamdocuk@gmail.com>
2018-05-30 16:04:22 +0200

    add search, not quite working

--- star-wars/src/App.js
+++ star-wars/src/App.js
@@ -1,3 +1,5 @@
+// https://swapi.co/api/people/1/
+
 import React, { Component } from 'react';
 import './App.css';
 import Header from "./components/header";
@@ -5,33 +7,44 @@ import ContainerName from "./components/containerName";
 import BasicDetails from "./components/basicDetails";
 import World from "./components/containerWorld";
 import Films from "./components/containerFilms";
+import SearchBar from "./components/searchBar";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-     url: 'https://swapi.co/api/people/1/',
+     url: '',
      person: '',
      world: [],
      species: [],
      films: '',
-    }; 
+    };
   }
 
   async componentDidMount() {
     const res = await fetch(this.state.url);
     const person = await res.json();
-    this.setState({person});
+    await this.setState({person});
      
     const resWorld = await fetch(this.state.person.homeworld);
     const world = await resWorld.json();
-    this.setState({world});
+    await this.setState({world});
      
     const resSpecies = await fetch(this.state.person.species);
     const species = await resSpecies.json();
     this.setState({species});
     
+  }
+
+  async search(input) {
+    let searchURL = `https://swapi.co/api/people/?search=+${input}`;
+    console.log(searchURL)
+
+    const res = await fetch(searchURL);
+    const data = await res.json();
+    this.setState({url: data.results[0].url});
+    console.log(data.results[0].url)
   }
 
  // getFilms(data) {
@@ -46,6 +59,7 @@ class App extends Component {
     return (
       <div>
         <Header />
+        <SearchBar handleSearch={this.search}/>
         <ContainerName name={ this.state.person.name } />
         <BasicDetails person={this.state.person} species={this.state.species.name}/>
         <World world={this.state.world} />
---
+++ star-wars/src/components/searchBar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+	render() {
+		return (
+			<form className="search-container" onSubmit={this.eventHandler}>
+				<input 
+			   		id="searchTerm"
+			    	type="text" 
+			    	placeholder="  Type Star Wars Character here..."
+			    />
+				<input type="submit" value="Punch it!" />
+			</form>
+			);
+		}
+
+	eventHandler = e => {
+		this.props.handleSearch(e.target[0].value)
+		e.preventDefault();
+	}
+}
+
+export default SearchBar;
+
